0cd6e2c53525e758c6a361f3431e4ed33d42f0f5
Kurt Arnlund <kurt@iatapps.com>
2019-10-22 15:42:14 -0700

    remove duplicate dealloc that resulted from merge

--- classes/PhAuthenticationToken.m
+++ classes/PhAuthenticationToken.m
@@ -44,14 +44,6 @@
     [coder encodeObject:self.permissions forKey:@"permissions"];
 }
 
-- (void) dealloc
-{
-    [_authenticationToken release];
-    [_expiry release];
-    [_permissions release];
-    [super dealloc];
-}
-
 #pragma mark Initialization
 
 - (id) initWithToken: (NSString*) token secondsToExpiry: (NSTimeInterval) seconds permissions: (NSString*) perms
